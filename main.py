7b4163a1fed74084e5c0cf5d6b1dc8756f956b0f
mrhidethepain <157283691+mrhidethepain@users.noreply.github.com>
2024-03-03 12:52:51 +0100

    2

---
+++ main.py
@@ -0,0 +1,20 @@
+a= 56
+print(a)
+print(type(a))
+b= 5.5
+print(b)
+print(type(b))
+zmienna_tekstowa = 'wizualizacja danych'
+print(zmienna_tekstowa)
+print(type(zmienna_tekstowa))
+
+x=6
+y=3
+z=x+y
+print(z)
+q=x-y
+print(q)
+e=y//x
+print(e)
+f=x//y
+print(f)
